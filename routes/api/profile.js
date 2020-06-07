f01e51f8154e26525e9551c71aa0857eb382ec7a
pernybom <per@znipe.se>
2020-06-07 21:25:11 +0200

    WIP 20

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -153,4 +153,20 @@ router.delete('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+//@route  PUT api/profiles/experiences
+//@desc   Add profile experience
+//@access Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {}
+);
 module.exports = router;
